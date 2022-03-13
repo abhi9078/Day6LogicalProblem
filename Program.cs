9a9bd31057377fa914755925f9a06ef747e5859d
Abhilash <abhilashmeher1234@gmail.com>
2022-03-13 09:52:10 +0530

    Added Fibonacci numbber series program file

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Day6LogicalProblem
+{
+    internal class Program
+    {
+        public static void Fibonacci()
+        {
+            int num1 = 0, num2 = 1, temp, i, number;
+            Console.Write("Enter the number of elements: ");
+            number = int.Parse(Console.ReadLine());
+            Console.Write(num1 + " " + num2 + " ");
+            for (i = 2; i < number; ++i)
+            {
+                temp = num1 + num2;
+                Console.Write(temp + " ");
+                num1 = num2;
+                num2 = temp;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Welcome to Fibonacci Series");
+            Program.Fibonacci();
+        }
+    }
+}
